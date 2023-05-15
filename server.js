847ece7d2fb1f85f42feb77eae8cf8f145eda473
Zack Miller <126508376+ZmCoder2@users.noreply.github.com>
2023-05-15 18:35:59 -0400

    insomnia working

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const path = require("path");
-const PORT = process.env.PORT || 3001;
+const PORT = 3001;
 const app = express();
+const controllers = require('./controllers')
+
+app.use(controllers);
+
 
 app.listen(PORT, () => {
     console.log('Hey Guys')
 });
 
-.
