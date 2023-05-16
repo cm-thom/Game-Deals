53157d2b63d69ed048a436c026fb269e932d5f24
Cole <122782745+cm-thom@users.noreply.github.com>
2023-05-15 20:20:52 -0400

    added handlebars. added public. updated controllers to route / to homepage.handlebars.

--- server.js
+++ server.js
@@ -1,14 +1,29 @@
 const express = require('express');
 const path = require("path");
-const PORT = 3001;
-const app = express();
 const controllers = require('./controllers')
 const sequelize = require('./config/connection');
+const exphbs = require('express-handlebars');
 
+const PORT = 3001;
+const app = express();
 
+//needed so express can find public (front-end) assets
+app.use(express.static('public'));
 
+//you don't need to include this if you don't have helper functions.
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+//needed for POST requests
+app.use(express.json());
+app.use(express.urlencoded( { extended: true }));
+
+//needed so sequelize can find the models
 const models = require('./models');
 
+//needed so express can find the routes
 app.use(controllers);
 
 sequelize.sync({force: false}).then(() => {
