0de44d09e910f1ffc1f9eee5829eb1f72d5e4f23
Cole <122782745+cm-thom@users.noreply.github.com>
2023-05-18 14:11:43 -0400

    deleted non-functioning 3-bar menus next to login and sign up buttons.

--- controllers/search/index.js
+++ controllers/search/index.js
@@ -5,28 +5,28 @@ const Op = Sequelize.Op;
 
 // localhost:3001/search/
 
-router.get('/', async (req, res) => {
-    try {
-        const userQuery = 'sonic';
+// router.get('/', async (req, res) => {
+//     try {
+//         const userQuery = 'sonic';
     
-        const products = await Products.findAll({
-          where: {
-            name: userQuery
-          }
-        });
+//         const products = await Products.findAll({
+//           where: {
+//             name: userQuery
+//           }
+//         });
     
-        const results = products.map((product) =>
-          product.get({ plain: true })
-          );
-        console.log(results);
+//         const results = products.map((product) =>
+//           product.get({ plain: true })
+//           );
+//         console.log(results);
   
-        res.render('search', results[0]);
+//         res.render('search', results[0]);
     
-      } catch (error) {
-        console.log(error);
-        res.status(500).json({ msg: 'Internal server error' });
-      }
-  });
+//       } catch (error) {
+//         console.log(error);
+//         res.status(500).json({ msg: 'Internal server error' });
+//       }
+//   });
 
 
 router.get('/:userQuery', async (req, res) => {
